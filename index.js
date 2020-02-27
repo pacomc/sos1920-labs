2bf8c1b00ce2f0496b7bbb187b2d7dc5e828aa93
pacomc <pacomc99@gmail.com>
2020-02-27 09:24:16 +0000

    history file created l04

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var express = require("express");
+
 var cool = require("cool-ascii-faces");
 
 
-console.log(cool());
+var app = express();
+
+app.get("/recurso", (req, res) => {
+	res.send("<html><body>Hello world!</body></html>");
+});
+
+app.listen(80);
+
+console.log("Server ready!");
