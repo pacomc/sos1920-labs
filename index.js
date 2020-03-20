56513d8e5e6d0b354ae89873f0453e948dfe5266
pacomc <pacomc99@gmail.com>
2020-03-20 09:59:16 +0000

    final l06

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(bodyParser.json()); // te traduce automaticamente a json
+app.use(bodyParser.json()); // todo lo que te llega a la api lo traduce a json automaticamente
 
 var port = process.env.PORT || 80;
 
@@ -17,13 +17,41 @@ app.use("/",express.static("./public"));
 
 var contacts = [
 	{
-		name: "peter",
-		phone: 12345
+		"country": "Spain",
+		"year": "2016",
+		"percentage-re-total": "38.1",
+		"percentage-hydropower-total": "14.5",
+		"percentage-wind-power-total": "17.8"	
 	},
 	{
-		name: "pablo",
-		phone: 78910
+		"country": "France",
+		"year": "2016",
+		"percentage-re-total": "17.5",
+		"percentage-hydropower-total": "11.7",
+		"percentage-wind-power-total": "3.8"	
+	},
+	{
+		"country": "Russia",
+		"year": "2016",
+		"percentage-re-total": "16.9",
+		"percentage-hydropower-total": "17.0",
+		"percentage-wind-power-total": "0.0"	
+	},
+	{
+		"country": "Canada",
+		"year": "2016",
+		"percentage-re-total": "65.0",
+		"percentage-hydropower-total": "58.0",
+		"percentage-wind-power-total": "4.6"	
+	},
+	{
+		"country": "Greece",
+		"year": "2016",
+		"percentage-re-total": "27.4",
+		"percentage-hydropower-total": "42.7",
+		"percentage-wind-power-total": "9.5"	
 	}
+	
 ];
 
 const BASE_API_URL =  "/api/v1";
@@ -34,7 +62,7 @@ app.get(BASE_API_URL + "/contacts", (req, res) => {
 
 app.post(BASE_API_URL + "/contacts", (req, res) => {
 	contacts.push(req.body);
-	res.sendStatus(200, "OK");
+	res.sendStatus(201, "CREATED");
 });
 
 app.listen(port, () => {
