bc570fc6f3bea5bf092e11ae1b25ce03c7a3dc23
pacomc <pacomc99@gmail.com>
2020-03-20 10:06:58 +0000

    inicio l07

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cool = require("cool-ascii-faces");
+// Llamamos a los modulos
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,13 +7,6 @@ var app = express();
 app.use(bodyParser.json()); // todo lo que te llega a la api lo traduce a json automaticamente
 
 var port = process.env.PORT || 80;
-
-
-app.get("/cool",(request,response) => {
-	response.send("<html>"+cool()+"</html>");
-});
-
-app.use("/",express.static("./public"));
 
 var contacts = [
 	{
@@ -57,7 +50,7 @@ var contacts = [
 const BASE_API_URL =  "/api/v1";
 
 app.get(BASE_API_URL + "/contacts", (req, res) => {
-	res.send(JSON.stringify(contacts, null, 2)); // el tercer parametro es par aque lo indente
+	res.send(JSON.stringify(contacts, null, 2)); // el tercer parametro es para que lo indente
 });
 
 app.post(BASE_API_URL + "/contacts", (req, res) => {
